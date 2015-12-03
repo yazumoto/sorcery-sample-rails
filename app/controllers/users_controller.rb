99ce8033aed85376697a58726301f5ecd5091b68
Yassun <tyasumoto@coiney.com>
2015-12-03 18:21:56 +0900

    modified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:index, :new, :create]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -28,8 +29,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        # format.html { redirect_to @user, notice: 'User was successfully created.' }
+        # format.json { render :show, status: :created, location: @user }
+        redirect_to(:users, notice: 'User was successfully created')
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
